06221e7a27aca7e0be5fca5f30d65b1c80b92bc7
liuhaonan <acerola.orion@foxmail.com>
2020-03-24 21:47:16 +0800

    feat(#frontend): add admin page

---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,102 @@
+const Home = {
+    template: `
+<div>
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">注册人数</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{counter.registered}}</div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">激活人数</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{counter.actived}}</div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-comments fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">在线人数</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{counter.online}}</div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">文档个数</div>
+                  <div class="row no-gutters align-items-center">
+                    <div class="col-auto">
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{counter.document}}</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+`,
+    data () {
+        return {
+            counter: {
+                registered: 52136,
+                actived: 50149,
+                online: 1563,
+                document: 22871
+            }
+        }
+    }
+};
+const DocInfo = {
+    template: `<div>bbb</div>`
+};
+const DocRecord = {
+    template: `<div>ccc</div>`
+};
+const DocTrace = {
+    template: `<div>ddd</div>`
+};
+const UserInfo = {
+    template: `<div>eee</div>`
+};
+const UserRecord = {
+    template: `<div>fff</div>`
+};
+
+
